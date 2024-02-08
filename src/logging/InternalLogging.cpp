44d660461fac77590e8e96f56e4943bd13e80d1c
underscore95 <95underscore@gmail.com>
2024-02-08 10:57:29 +0000

    Now using conditional_variable instead of loop with sleep

--- src/logging/InternalLogging.cpp
+++ src/logging/InternalLogging.cpp
@@ -7,47 +7,47 @@
 #include <magic_enum.hpp>
 
 namespace SparkInternal::Logging {
+
 	// Blocking function, will handle all the logs in queue
 	void handleLogs();
 
 	std::queue<SparkInternal::Logging::Log> logQueue;
 	std::mutex logQueueMutex;
+	std::condition_variable logQueueCondition;
 
 	std::thread thread(handleLogs);
 
-	void handleLogs()
-	{
+	void handleLogs() {
 		thread.detach();
-        while (true) {
-            logQueueMutex.lock();
-            if (logQueue.empty()) {
-                logQueueMutex.unlock();
-                std::this_thread::sleep_for(std::chrono::milliseconds(25));
-            }
-            else {
-                const auto log = std::move(logQueue.front());
-                logQueue.pop();
-                logQueueMutex.unlock();
+		while (true) {
+			std::unique_lock<std::mutex> lock(logQueueMutex);
+			logQueueCondition.wait(lock, [] { return !logQueue.empty(); });
 
-                const std::string currentTime = Spark::Utils::getDateTimeString();
-                const std::string logLevelName = log.debug ? "DEBUG" : std::string(magic_enum::enum_name(log.level));
-                const std::string logString = "[" + currentTime + "] [" + logLevelName + "] " + "[" + log.loggerName + "] " + log.message;
+			const auto log = std::move(logQueue.front());
+			logQueue.pop();
 
-                std::cout << "3" << logString;
+			lock.unlock();
 
-                if (log.level >= Spark::Logging::LogLevel::SEVERE) {
-                    std::cout << std::endl;
-                }
-                else {
-                    std::cout << "\n";
-                }
-            }
-        }
+			const std::string currentTime = Spark::Utils::getDateTimeString();
+			const std::string logLevelName = log.debug ? "DEBUG" : std::string(magic_enum::enum_name(log.level));
+			const std::string logString = "[" + currentTime + "] [" + logLevelName + "] " + "[" + log.loggerName + "] " + log.message;
+
+			std::cout << "4" << logString;
+
+			if (log.level >= Spark::Logging::LogLevel::SEVERE) {
+				std::cout << std::endl;
+			}
+			else {
+				std::cout << "\n";
+			}
+		}
 	}
-    void log(const std::chrono::system_clock::time_point& time, const Spark::Logging::LogLevel& level, const std::string& loggerName, const std::string& message, const bool debug)
-    {
-        logQueueMutex.lock();
-        logQueue.emplace(time, level, loggerName, message, debug);
-        logQueueMutex.unlock();
-    }
+
+	void log(const std::chrono::system_clock::time_point& time, const Spark::Logging::LogLevel& level, const std::string& loggerName, const std::string& message, const bool debug) {
+		{
+			std::lock_guard<std::mutex> lock(logQueueMutex);
+			logQueue.emplace(time, level, loggerName, message, debug);
+		}
+		logQueueCondition.notify_one();
+	}
 }
