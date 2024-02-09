ed3cf8a90298ef587fa9969ba205081c1222c8a7
underscore95 <95underscore@gmail.com>
2024-02-09 12:09:53 +0000

    Added PCH for std

---
+++ src/precompiled_headers/std.h
@@ -0,0 +1,23 @@
+#pragma once
+
+// Data Structures
+
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <stack>
+#include <string>
+
+// Multithreading
+
+#include <mutex>
+#include <thread>
+
+// Useful things
+
+#include <algorithm>
+#include <iostream>
+#include <memory>
