78109bf6bd3b2231d109037721d6a89c5db6a0ce
underscore95 <95underscore@gmail.com>
2024-02-10 14:04:34 +0000

    GLFW Windows

---
+++ src/window/abstract/Window.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "std.h"
+#include <glm/glm.hpp>
+
+namespace Spark::Window {
+	class Window {
+	private:
+
+	protected:
+		std::string title;
+		glm::uvec2 dimensions;
+		bool successfullyInitialised{ false };
+		bool windowClosed{ false };
+
+		virtual bool createWindow() = 0;
+
+	public:
+		Window(const std::string& title, const glm::uvec2& dimensions = glm::uvec2(1280, 720));
+		Window(const Window& copy) = delete;
+
+		virtual void update() = 0;
+
+		// Returns true if the window was successfully initialised and opened
+		const bool isSuccessfullyInitialised() const;
+		const std::string& getTitle() const;
+		const glm::uvec2 getDimensions() const;
+		// Returns true if the window has been opened and is now closed
+		const bool isWindowClosed() const;
+		// Returns true if the window is currently open, returns false if the window has been closed or if the window was never opened
+		const bool isWindowOpen() const;
+
+		virtual void setTitle(const std::string& title) = 0;
+		virtual void setDimensions(const glm::uvec2& dimensions) = 0;
+	};
+}
---
+++ src/window/glfw/GLFWWindow.cpp
@@ -0,0 +1,74 @@
+#include "GLFWWindow.h"
+#include "logging.h"
+
+void errorCallback(int error, const char* description)
+{
+	auto& logger = Spark::Logging::getLogger("spark");
+	std::string message;
+	std::stringstream ss;
+	ss << "glfw error occurred. (Error Code: " << error << ")\n" << description;
+	ss.str(message);
+	logger.severe(message);
+}
+
+bool Spark::Window::GLFWWindow::createWindow()
+{
+	auto& logger = Spark::Logging::getLogger("spark");
+	glfwSetErrorCallback(errorCallback);
+
+	if (!glfwInit()) {
+		logger.severe("Failed to initialise GLFW");
+		return false;
+	}
+
+	window = glfwCreateWindow(dimensions.x, dimensions.y, title.c_str(), NULL, NULL);
+	if (!window) {
+		logger.severe("Failed to create GLFW window");
+		glfwTerminate();
+		return false;
+	}
+
+	glfwMakeContextCurrent(window);
+	return true;
+}
+
+Spark::Window::GLFWWindow::GLFWWindow(const std::string& title, const glm::uvec2& dimensions) : Window(title)
+{
+	this->successfullyInitialised = createWindow();
+}
+
+Spark::Window::GLFWWindow::~GLFWWindow()
+{
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void Spark::Window::GLFWWindow::update()
+{
+	if (isWindowClosed()) return;
+
+	if (glfwWindowShouldClose(window)) {
+		windowClosed = true;
+		return;
+	}
+
+	glfwSwapBuffers(window);
+
+	glfwPollEvents();
+}
+
+void Spark::Window::GLFWWindow::setTitle(const std::string& title)
+{
+	if (isWindowOpen()) {
+		this->title = title;
+		glfwSetWindowTitle(window, this->title.c_str());
+	}
+}
+
+void Spark::Window::GLFWWindow::setDimensions(const glm::uvec2& dimensions)
+{
+	if (isWindowOpen()) {
+		this->dimensions = dimensions;
+		glfwSetWindowSize(window, dimensions.x, dimensions.y);
+	}
+}
