016d966d055dff801ffabee40d1cf1f63dd85e54
underscore95 <95underscore@gmail.com>
2024-02-14 10:18:51 +0000

    Added static assert for T

--- src/entities/ComponentTypeRegistry.h
+++ src/entities/ComponentTypeRegistry.h
@@ -3,27 +3,30 @@
 #include "pch.h"
 
 namespace SparkInternal::Entity {
-    // Define a type registry to map component types to unique identifiers
-    class ComponentTypeRegistry {
-    private:
-        static ComponentTypeRegistry* instance;
-        std::unordered_map<std::type_index, size_t> typeIds;
-    public:
-        static ComponentTypeRegistry& getInstance() {
-            if (!instance) instance = new ComponentTypeRegistry();
-            return *instance;
-        }
+	// Define a type registry to map component types to unique identifiers
+	class ComponentTypeRegistry {
+	private:
+		static ComponentTypeRegistry* instance;
+		std::unordered_map<std::type_index, size_t> typeIds;
+	public:
+		static ComponentTypeRegistry& getInstance() {
+			if (!instance) instance = new ComponentTypeRegistry();
+			return *instance;
+		}
 
-        template<typename T>
-        size_t getTypeId() {
-            auto typeIdIt = typeIds.find(typeid(T));
-            if (typeIdIt == typeIds.end()) {
-                // Generate a new type id for the component type
-                size_t newTypeId = typeIds.size();
-                typeIds[typeid(T)] = newTypeId;
-                return newTypeId;
-            }
-            return typeIdIt->second;
-        }
-    };
+		template<typename T>
+		size_t getTypeId() {
+			static_assert(std::is_base_of<BaseComponent, T>::value && !std::is_same<BaseComponent, T>::value,
+				"T must be a derived class of BaseComponent");
+
+			auto typeIdIt = typeIds.find(typeid(T));
+			if (typeIdIt == typeIds.end()) {
+				// Generate a new type id for the component type
+				size_t newTypeId = typeIds.size();
+				typeIds[typeid(T)] = newTypeId;
+				return newTypeId;
+			}
+			return typeIdIt->second;
+		}
+	};
 }
