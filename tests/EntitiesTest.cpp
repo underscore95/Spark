6f729b8d55a50284df8369430a9f427fb21b1e63
underscore95 <95underscore@gmail.com>
2024-02-16 11:57:12 +0000

    Removed main function from tests

---
+++ tests/EntitiesTest.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+//
+//#include "Spark.h"
+//#include "pch.h"
+//
+//class TestComponent : public Spark::Entity::BaseComponent {};
+
+TEST(Entities, addEntityGivesUniqueId) {
+    ASSERT_TRUE(true);
+}
