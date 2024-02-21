7059f8dbf44b5e98481a87afba4cabc6ae92ac52
underscore95 <95underscore@gmail.com>
2024-02-21 14:34:39 +0000

    Update Entity.h

--- src/entities/Entity.h
+++ src/entities/Entity.h
@@ -5,5 +5,6 @@
 namespace Spark::Entity {
 	struct Entity {
 		std::vector<Spark::Entity::BaseComponent*> components;
+		// TODO: bit vector for constant time has-all-components lookup (using a bit mask) (currently linear time)
 	};
 }
