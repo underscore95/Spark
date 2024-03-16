b4a923d7d9653b4d5b2c528b99851076789237d5
underscore95 <95underscore@gmail.com>
2024-03-16 13:43:14 +0000

    Added keyboard controller constructor

--- src/graphics/api/camera/KeyboardCameraController.h
+++ src/graphics/api/camera/KeyboardCameraController.h
@@ -6,5 +6,10 @@ namespace Spark::Graphics {
 	class KeyboardCameraController : public CameraController {
 	protected:
 		virtual void handleInput(float dt) override;
+
+	public:
+		KeyboardCameraController(std::shared_ptr<Camera> camera, Spark::Window::Input& input, const float speed = 100.0f, const float sensitivity = 100.0f)
+			: CameraController(camera, input, speed, sensitivity) {}
+
 	};
 }
